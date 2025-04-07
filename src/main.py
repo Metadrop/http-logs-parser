13a960599681afc25bc1c47d37234d3be7109bd1
Jonathan Araña Cruz <jonathan.arana@metadrop.net>
2025-04-07 17:22:12 +0200

    Fix for built-in log formats

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.log_format in log_formats:
-        log_format = log_formats[args.log_format]
+        log_format = '"%s"' % log_formats[args.log_format]
     else:
         log_format = args.log_format
 
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     cmd += ["goaccess"]
     cmd += ["--all-static-files"]
     cmd += [f"--datetime-format={args.datetime_format}"]
-    cmd += [f'--log-format="{log_format}"']
+    cmd += [f'--log-format={log_format}']
     cmd += [f"--output={args.report_file}"]
     cmd += [args.log_file]
 
